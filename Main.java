30a253ef47d7d9ff06dc5ae4a7245327e9c36b1e
Darael Badilla <daraelbadilla11@gmail.com>
2025-01-28 21:44:07 -0300

    Beggining

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Java.Proyecto.src.TaskManager;
+
+class Main {
+    static int x = 5;
+    static int y = 10;
+    static int a =x+y; 
+    public static void main(String args[]){
+        
+        System.out.println("Hola Mundo: " + a);
+    }
+}
